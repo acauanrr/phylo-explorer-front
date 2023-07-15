878180f7c0e61125e59e27de5e4d7ba315910169
Acauan Ribeiro <acauan.ribeiro@gmail.com>
2023-07-15 12:54:39 -0400

    ajeitando next-config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: false,
+  assetPrefix: "/phylo",
+  publicRuntimeConfig: {
+    basePath: "/phylo",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
